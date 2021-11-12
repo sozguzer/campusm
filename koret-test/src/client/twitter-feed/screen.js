2a79c30820738d116a2b3992d0f3db2b70c53c46
Selin Ozguzer <sozguzer@usfca.edu>
2021-11-12 15:44:01 -0800

    revert to mine

--- koret-test/src/client/twitter-feed/screen.js
+++ koret-test/src/client/twitter-feed/screen.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {
   Container,
-  VBox,
-  BannerHeader,
-  BasicSegment,
+  VBox
 } from "@ombiel/aek-lib";
 import {Timeline} from 'react-twitter-widgets'; 
+import styles from './styles.module.css';
 
 export default class Screen extends React.Component {
   componentDidMount() {}
@@ -14,20 +13,35 @@ export default class Screen extends React.Component {
     return (
       <Container>
         <VBox>
-          <BannerHeader theme="alt" key="header" data-flex={0}>
-            Twitter Feed
-          </BannerHeader>
-          <BasicSegment>
-            <Timeline
+          <React.Fragment>
+            <img className={styles.heroImage} src="https://portal-na.campusm.exlibrisgroup.com/assets/UniversityofSanFrancisco/UniversityofSanFrancisco/images/koret.jpg" alt="people swimming in the pool" />           
+            <ul className={styles.listLinks}>
+              <li><a href="https://www.usfca.edu/koret/fitness/group-exercise/schedule" title="Group Exercise Schedule">Group Exercise Schedule
+                  <img src="https://portal-na.campusm.exlibrisgroup.com/assets/UniversityofSanFrancisco/UniversityofSanFrancisco/images/arrow.png" alt="arrow" className={styles.arrow} /></a></li>
+              <li><a href="" title="Daily Exercise Schedule">Daily Exercise Schedule
+                  <img src="https://portal-na.campusm.exlibrisgroup.com/assets/UniversityofSanFrancisco/UniversityofSanFrancisco/images/arrow.png" alt="arrow" className={styles.arrow} /></a></li>
+              <li><a href="" title="Clubs & Intramural Sports Schedule">Clubs & Intramural Sports Schedule
+                  <img src="https://portal-na.campusm.exlibrisgroup.com/assets/UniversityofSanFrancisco/UniversityofSanFrancisco/images/arrow.png" alt="arrow" className={styles.arrow} /></a></li>
+              <li><a href="https://myusf.usfca.edu/koret/fitness" title="Fitness 101">Fitness 101
+                  <img src="https://portal-na.campusm.exlibrisgroup.com/assets/UniversityofSanFrancisco/UniversityofSanFrancisco/images/arrow.png" alt="arrow" className={styles.arrow} /></a></li>
+            </ul><br />
+          
+          <div className={styles.twitterHeader}>
+            KORET TWITTER
+          </div>
+          <div className={styles.timeline}>
+            <Timeline 
               dataSource={{
                 sourceType: 'profile',
-                screenName: 'ExLibrisGroup'
+                screenName: 'usfkoret'
               }}
               options={{
-                height: '420'
+                height: '420',
+                width: '90%'
               }}
             />
-          </BasicSegment>
+          </div>
+         </React.Fragment>
         </VBox>
       </Container>
     );
